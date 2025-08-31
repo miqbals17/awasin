072c88561aec7cca3c56ee6b7254299b850ea88c
Muhammad Iqbal S <yakuzuyoa@gmail.com>
2025-08-31 19:02:12 +0700

    fix: add dummy circle

--- src/components/heatmap.tsx
+++ src/components/heatmap.tsx
@@ -1,16 +1,22 @@
 import { useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { Circle, MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import type { LatLngExpression, PathOptions } from "leaflet";
 
-const LATITUDE_JAKARTA = -6.2085967;
-const LONGITUDE_JAKARTA = 106.8025844;
+const LATITUDE_DPR = -6.2085967;
+const LONGITUDE_DPR = 106.8025844;
+
+const DPR_AREA: LatLngExpression = [LATITUDE_DPR, LONGITUDE_DPR];
+const POLDA_AREA: LatLngExpression = [-6.224071, 106.8093165];
+
+const fillRedOptions: PathOptions = { fillColor: "red", color: "" };
 
 export default function Heatmap() {
   const mapRef = useRef(null);
 
   return (
     <MapContainer
-      center={[LATITUDE_JAKARTA, LONGITUDE_JAKARTA]}
+      center={[LATITUDE_DPR, LONGITUDE_DPR]}
       zoom={14}
       ref={mapRef}
       style={{ height: "75%", width: "100%" }}
@@ -19,6 +25,8 @@ export default function Heatmap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Circle center={DPR_AREA} pathOptions={fillRedOptions} radius={2000} />
+      <Circle center={POLDA_AREA} pathOptions={fillRedOptions} radius={450} />
     </MapContainer>
   );
 }
